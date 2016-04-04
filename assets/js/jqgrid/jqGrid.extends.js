15dbcce7a183c65540d89cf5337eb6e42d61cf67
deller <malashinr@mte-telecom.ru>
2016-04-04 20:29:59 +0300

    fix rows

--- assets/js/jqgrid/jqGrid.extends.js
+++ assets/js/jqgrid/jqGrid.extends.js
@@ -9,3 +9,15 @@ NNX.jqGrid.expandCollapseAll = function (grid) {
         $(this).trigger('click');
     });
 };
+
+NNX.jqGrid.indent = function (grid) {
+    var rootNodes = grid.getRootNodes();
+    var fullTreeNode, k;
+    for (var i = 0; i < rootNodes.length; i++) {
+        fullTreeNode = grid.getFullTreeNode(rootNodes[i]);
+        console.log(fullTreeNode);
+        for (k = 0; k < fullTreeNode.length; k++) {
+            $('#' + fullTreeNode[k]._id_).find('.cell-wrapper, .cell-wrapperleaf').css('margin-left', (parseInt(fullTreeNode[k].level) + 1) * 18);
+        }
+    }
+};
